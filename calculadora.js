2a5a8cf8109bf9461f7f3918255736a5536c17b4
Fervic Paredes <fervicparedes@gmail.com>
2023-01-18 10:14:06 -0400

    general function: it made logic structuring of operations add, less, multiplicatión and divitión

--- calculadora.js
+++ calculadora.js
@@ -8,7 +8,7 @@ const calculator = {
   five: document.getElementById("five"),
   six: document.getElementById("six"),
   seven: document.getElementById("seven"),
-  eigth: document.getElementById("eigth"),
+  eigth: document.getElementById("eight"),
   nine: document.getElementById("nine"),
   zero: document.getElementById("zero"),
   less: document.getElementById("less"),
@@ -23,20 +23,82 @@ const calculator = {
   //000000000000000000000000000000000000000000000000000
 
   //events
-  addNumber: (e) => {
-    console.log(e.target);
-  },
-  // addNumber: () => {
-  //   console.log("this");
-  // },
+  addToScreen: function (character) {
+    let screenChars = this.screen.innerText;
+    let lastCharacter = this.screen.innerText.slice(-1);
 
-  events: () => {
-    one.addEventListener("click", (e) => {
-      calculator.addNumber(e);
-    });
+    let newCharacter = character;
+    console.log("entré1");
+    console.log(newCharacter);
+    // console.log(/[X/+-=]/i.test(newCharacter)); //duda que tengo que solventar
+    // console.log(/[+X/=-]/i.test(newCharacter));
+
+    // first and main conditional to evit operators whitout operand (with empty screen)
+    if (!(screenChars === "" && /[+X/=-]/i.test(newCharacter))) {
+      // second conditional to evit operators super posing
+      console.log("entré2");
+      if (/[+X/=-]/i.test(lastCharacter) && /[+X/=-]/i.test(newCharacter)) {
+        screenChars = screenChars.replace(lastCharacter, newCharacter);
+        console.log("segundo conditional");
+        this.screen.innerText = screenChars;
+      } else if (/[0-9]/.test(newCharacter)) {
+        this.screen.innerText += newCharacter; //here a number is added to the screen
+      } else {
+        this.screen.innerText += newCharacter;
+        console.log("aqui llegue con el boton igual");
+      }
+    }
   },
+
+  result: function () {
+    let result = "";
+    console.log("boton igual");
+    console.log(result);
+
+    if (/X/i.test(this.screen.innerText)) {
+      result = this.screen.innerText.replace(/x/gi, "*");
+    }
+
+    result = eval(result);
+    console.log(result);
+    this.screen.innerText = result;
+  },
+
+  cleanerScreen: function () {
+    this.screen.innerText = "";
+    console.log("estoy en cleaner");
+  },
+
+  //
 
   //end events
   //000000000000000000000000000000000000000000000000000
 };
-calculator.events();
+
+const assigmentEvents = () => {
+  for (const properties in calculator) {
+    if (/[^=c]/i.test(calculator[properties].innerText)) {
+      console.log(calculator[properties].innerText);
+      calculator[properties].addEventListener("click", function (element) {
+        let character = element.target.innerText;
+        calculator.addToScreen(character);
+      });
+    } else if (calculator[properties].innerText === "=") {
+      console.log(calculator[properties].innerText);
+      calculator[properties].addEventListener("click", function () {
+        calculator.result();
+      });
+    } else if (calculator[properties].innerText === "C") {
+      console.log("estoy con la letra C");
+      calculator[properties].addEventListener("click", function () {
+        calculator.cleanerScreen();
+      });
+    }
+  }
+
+  console.log(this);
+};
+
+//
+assigmentEvents();
+// calculator.metodo();
